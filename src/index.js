c2362c9c449b6e6e8c7b5ad1d3212893b67ac06c
FloRida <f.becherer@gmx.de>
2018-07-05 16:24:31 +0200

    Added first Version of React with state management für button counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+const e = React.createElement;
+
+class HelloState extends React.Component {
+  constructor() {
+    super();
+    this.state = { home: 0, away: 0 };
+    this.incrementHome = () => {
+      this.setState(state => ({
+        home: state.home + 1
+      }));
+    };
+    this.decrementHome = () => {
+      this.setState(state => ({
+        home: state.home - 1
+      }));
+    };
+    this.incrementAway = () => {
+      this.setState(state => ({
+        away: state.away + 1
+      }));
+    };
+    this.decrementAway = () => {
+      this.setState(state => ({
+        away: state.away - 1
+      }));
+    };
+  }
+
+  render() {
+    const { home } = this.state;
+    const { away } = this.state;
+    return (
+      <div className='results'>
+        <button onClick={this.incrementHome}>+</button>
+        <button onClick={this.decrementHome}>-</button>
+        <div>
+          {" "}
+          {home} : {away}
+        </div>
+        <button onClick={this.incrementAway}>+</button>
+        <button onClick={this.decrementAway}>-</button>
+        <button onClick={this.incrementHome}>+</button>
+        <button onClick={this.decrementHome}>-</button>
+        <div>
+          {" "}
+          {home} : {away}
+        </div>
+        <button onClick={this.incrementAway}>+</button>
+        <button onClick={this.decrementAway}>-</button>
+      </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector("#results");
+ReactDOM.render(e(HelloState), domContainer);
